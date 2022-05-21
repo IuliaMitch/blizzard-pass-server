ec735dfa42826804a5eee7f6c930fdcfb3af54cb
IuliaMitch <noipasz@gmail.com>
2022-05-21 01:26:14 -0300

    chore(app): implement endpoint for app status

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { GenderModule } from './gender/gender.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [GenderModule],
   controllers: [AppController],
   providers: [AppService],
 })

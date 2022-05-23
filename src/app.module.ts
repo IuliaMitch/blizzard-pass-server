1fabaffa70f10792439743cc0aaf10e5a2265146
IuliaMitch <noipasz@gmail.com>
2022-05-23 13:46:25 -0300

    feat(prisma): create module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { GenderModule } from './gender/gender.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { GenderController } from './gender/gender.controller';
+import { GenderService } from './gender/gender.service';
 
 @Module({
-  imports: [GenderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GenderModule, PrismaModule],
+  controllers: [AppController, GenderController],
+  providers: [AppService, GenderService],
 })
 export class AppModule {}

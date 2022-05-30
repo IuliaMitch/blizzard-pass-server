49455179c4fb3dbf279b0a868f18fa4eef6d56bf
IuliaMitch <noipasz@gmail.com>
2022-05-30 04:53:39 -0300

    feat(user): att entity and dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+
+export class CreateUserDto {
+    @ApiProperty({
+        description: 'Nome do usuário',
+        example: 'Iulia Mitch'
+    })
+    name: string
+    @ApiProperty({
+        description: 'Email do usuário',
+        example: 'noipasz@gmail.com'
+    })
+    email: string 
+    @ApiProperty({
+        description: 'Senha do usuário',
+        example: 'im54452323'
+    })
+    password: string
+    @ApiProperty({
+        description: 'CPF do usuário'
+    })
+    cpf: number
+    @ApiProperty({
+        description: 'Se o usuário é um ADM',
+        example: true
+    })
+    isAdmin: boolean
+}

e13ca19e3d4282481ebf70baba682ac163f838ef
IuliaMitch <noipasz@gmail.com>
2022-05-30 20:21:50 -0300

    feat(profile): implement `CreateDto`

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProfileDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl, IsUUID } from "class-validator";
+
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do perfil',
+    example: 'matry_xD'
+  })
+  title: string
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil',
+    example: 'https://i.pinimg.com/564x/7f/ec/3a/7fec3ae10ce408d91264e6f1a116d858.jpg'
+  })
+  imageUrl: string
+
+  @IsUUID()
+  @ApiProperty({
+    description: 'Id do usuário',
+    example: ''
+  })
+  userId: string;
+
+  @IsUUID()
+  @ApiProperty({
+    description: 'Gênero do jogo',
+    example: ''
+  })
+  genderName: string;
+
+  @IsUUID(undefined, {each: true})
+  @ApiProperty({
+    description: 'Jogos',
+    example: '',
+  })
+  games: string[]
+}

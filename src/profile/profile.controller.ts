cc381902f93c7de551ba2142af14871f504f2643
IuliaMitch <noipasz@gmail.com>
2022-05-30 20:10:20 -0300

    feat(profile): implement `Controller`

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('profile')
 @Controller('profile')
@@ -10,27 +10,42 @@ export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar um perfil'
+  })
   create(@Body() createProfileDto: CreateProfileDto) {
     return this.profileService.create(createProfileDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todos os perfis'
+  })
   findAll() {
     return this.profileService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Visualizar um perfil por ID'
+  })
   findOne(@Param('id') id: string) {
-    return this.profileService.findOne(+id);
+    return this.profileService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um perfil por ID'
+  })
   update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
-    return this.profileService.update(+id, updateProfileDto);
+    return this.profileService.update(id, updateProfileDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.profileService.remove(+id);
+  @ApiOperation({
+    summary: 'Remover um perfil por ID'
+  })
+  delete(@Param('id') id: string) {
+    return this.profileService.delete(id);
   }
 }

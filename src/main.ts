6b455a95e7473db3e3bf4157485e553391368f56
IuliaMitch <noipasz@gmail.com>
2022-05-30 04:55:55 -0300

    feat(user): implement controller

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   .addTag('status')
   .addTag('gender')
   .addTag('games')
+  .addTag('user')
   .build()
 
   const document = SwaggerModule.createDocument(app, config)
